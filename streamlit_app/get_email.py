5a481d278226347ffa4c5e67cb9552c71cd8fd99
Bossou Chiegueu Aryl Shamir <arylshamir98@gmail.com>
2025-12-09 15:19:56 +0100

    Corresponding Files

---
+++ streamlit_app/get_email.py
@@ -0,0 +1,50 @@
+import streamlit as st 
+import sys 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR/'scripts'))
+
+from email_structure import (load_email, get_email_structure,
+                                             structure_counter, html_to_plain_text,
+                                             email_to_text, email_language,
+                                             translate_into_english
+                                             )
+    
+
+def email_parser(loaded_email):
+    with st.expander("Here is the email content"):
+        email_text = email_to_text(loaded_email)
+        st.write(email_text)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+# def email_parser(email_file):
+
+# def email_parser(uploaded_file):
+#     if uploaded_file is None:
+#         st.info("Awaiting email file(s)")
+#         return
+
+#     with st.expander("Here is the email content"):
+#         # load_email will handle UploadedFile correctly
+#         try:
+#             email_msg = load_email(uploaded_file)
+#         except Exception as e:
+#             st.error(f"Failed to parse uploaded file: {e}")
+#             return
+
+#         st.session_state['email_msg'] = email_msg  # optional
+#         email_text = email_to_text(email_msg)
+#         st.write(email_text)
